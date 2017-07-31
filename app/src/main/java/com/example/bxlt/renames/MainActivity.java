90fa737b756f62f5666c2c1891fe722bfc6b678a
Lrxc <837533730@qq.com>
2017-07-31 11:06:20 +0800

    添加key 修复6.0权限

--- app/src/main/java/com/example/bxlt/renames/MainActivity.java
+++ app/src/main/java/com/example/bxlt/renames/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.bxlt.renames;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -26,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText edt_need;
     private String want;
     private String need;
+    private final int PremissWhat1 = 001;
+    private boolean isPremiss;//是否有权限
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initView();
+        initPermissions();
     }
 
     private void initView() {
@@ -40,7 +48,42 @@ public class MainActivity extends AppCompatActivity {
         edt_need = (EditText) findViewById(R.id.edt_need);
     }
 
+    //申请权限
+    private void initPermissions() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            int perm1 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            int perm2 = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+            if (perm1 != PackageManager.PERMISSION_GRANTED || perm2 != PackageManager.PERMISSION_GRANTED) {//未获取权限
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    //当用户第一次申请拒绝时，再次申请该权限调用
+                    Toast.makeText(this, "必须权限，否则程序将异常", Toast.LENGTH_SHORT).show();
+                }
+                //申请权限
+                String[] strings = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE};
+                ActivityCompat.requestPermissions(this, strings, PremissWhat1);
+            } else {//已经获得了权限
+                isPremiss = true;//同步账号
+            }
+        } else {
+            isPremiss = true;//同步账号
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @android.support.annotation.NonNull String[] permissions, @android.support.annotation.NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PremissWhat1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {//授权成功
+            isPremiss = true;//同步账号
+        } else {//授权失败
+            Toast.makeText(this, "获取权限失败，请重新授权", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     public void btn_start(View view) {
+        if (!isPremiss) {
+            Toast.makeText(this, "授权失败，请重启软件", Toast.LENGTH_SHORT).show();
+            return;
+        }
         want = edt_want.getText().toString();
         need = edt_need.getText().toString();
 
@@ -52,12 +95,10 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "替换成的字段不能为空", Toast.LENGTH_SHORT).show();
             return;
         }
-
         initRxjava();
     }
 
     private void initRxjava() {
-
         show = ProgressDialog.show(this, "", "改名中...");
         Observable.create(new ObservableOnSubscribe<String>() {
             @Override
